1b85ea85dccfa16e5967fbd9fb203825b2d45edc
CodeBloodedMama <84911345+CodeBloodedMama@users.noreply.github.com>
2022-11-02 11:49:03 +0100

    får en fejl i IMigrator når jeg sætter migration til

--- EFCore/data/Context.cs
+++ EFCore/data/Context.cs
@@ -5,30 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using FacilityDbManager.Model;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata.Internal;
 
 namespace FacilityDbManager.data
 {
     public class Context : DbContext
         
     {
+
+
+        //Connectionstring has to be dockerized. RN its InMemory not recommended for EFCore
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsbuilder)
+        {
+
+            optionsbuilder.UseInMemoryDatabase(databaseName: "FacilityDb");
+        }
+
+
         //Each DbTabel will be matched to its database
         public DbSet<Facility> Facilities { get; set; } = null!;
-        
 
         public DbSet<Reservation> Reservations { get; set; } = null!;
 
         public DbSet<User> Users { get; set; } = null!;
 
 
-        //Connectionstring has to be dockerized. RN its InMemory not recommended for EFCore
-        protected override void OnConfiguring(DbContextOptionsBuilder dbContextInMemory)
-        {
-
-            //.UseInMemoryDatabase(databaseName)
-            dbContextInMemory.UseInMemoryDatabase("Server=db;Database=master;User=sa;Password=Your_password123;");
-        }
-      
     }
     
 }
