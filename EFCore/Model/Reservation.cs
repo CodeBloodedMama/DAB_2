d3f1957f8dee47d553af278812fdedee00b46081
CodeBloodedMama <84911345+CodeBloodedMama@users.noreply.github.com>
2022-11-02 12:15:55 +0100

    deleted id in facility

--- EFCore/Model/Reservation.cs
+++ EFCore/Model/Reservation.cs
@@ -9,12 +9,12 @@ namespace EFCore.Model
 {
     public class Reservation
     {
-        public int Id { get; set; }
+    
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
         public int UserId { get; set; }
         public int FaciltyId { get; set; }
         public User User { get; set; } = null!;
-        public Facility Facility { get; set; } = null!;
+
     }
 }
