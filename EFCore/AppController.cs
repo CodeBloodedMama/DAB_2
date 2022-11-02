1a0984855d5280260328a4a9b2bc197e73a5bfa9
Marcin Szymanek <202009418@post.au.dk>
2022-11-02 10:34:32 +0100

    Implemented UI class for display/get command Added AppController, which is the main controller class Implemented app commands : quit and help

---
+++ EFCore/AppController.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EFCore.data;
+using EFCore.UI;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore;
+
+public class AppController
+{
+    private Ui _ui = new();
+    private Context _context;
+    private bool _running = false;
+
+    public AppController()
+    {
+        _context = new Context();
+    }
+
+    public int Run()
+    {
+        _ui.Display("Welcome to FacilityDbManager\n" +
+                    "Enter 'h' for help\n");
+        _running = true;
+        while (_running)
+        {
+            try
+            {
+                string command = _ui.GetCommand();
+                HandleCommand(command);
+            }
+            catch (Exception e)
+            {
+                _ui.Display(e.Message + "\n");
+            }
+        }
+
+        return 0;
+    }
+
+    // Command handler
+    private void HandleCommand(string command)
+    {
+        switch (command)
+        {
+            case "h":
+            case "help":
+            {
+                _ui.DisplayHelp();
+                break;
+            }
+            case "q":
+            case "quit":
+            {
+                _ui.Display("Exiting application\n");
+                _running = false;
+                break;
+            }
+            case "a":
+            case "add":
+            {
+                throw new NotImplementedException();
+            }
+            case "g":
+            case "get":
+            {
+                throw new NotImplementedException();
+            }
+            case "s":
+            case "show":
+            {
+                throw new NotImplementedException();
+            }
+            default:
+                _ui.Display("Unrecognized command\n");
+                break;
+        }
+    }
+}
---
+++ EFCore/UI/UI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.UI
+{
+    public class Ui
+    {
+        public string GetCommand()
+        {
+            Console.WriteLine("Enter command: ");
+            string command = "";
+            command += Console.ReadLine();
+            return command;
+        }
+
+        public void DisplayHelp()
+        {
+            Display("Available commands: \n");
+            Display("[q, quit] : quit application\n");
+            Display("[s, show] : show database tables\n");
+            Display("[a, add] : add entry to the database\n");
+            Display("[g, get] : get an entry from the database");
+            Display("[h, help] : show this message");
+        }
+
+        public void Display(string text)
+        {
+            Console.Write(text);
+        }
+    }
+}
